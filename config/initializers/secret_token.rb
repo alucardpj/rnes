3e152a1a243371c73bda09688d87afe1306ac615
alucardpj <alucardpj@gmail.com>
2013-03-20 14:31:58 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rnes::Application.config.secret_token = '7a99149b1fcbe63fcb1fbe0fbb660005fad224822e280264e7cb4b3e0281e42cf561080892e5ac890d1b41d3c820889641a9ef2fb41f65903b7e1879868fbb28'
