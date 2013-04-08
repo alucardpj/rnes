b832d049921caf081a90fbea8b1dc8cedb79852f
alucardpj <alucardpj@gmail.com>
2013-04-08 16:35:15 +0800

    修改demo#index页面，测试页面布局

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,14 @@
 class ResultsController < ApplicationController
   def index
+    query = URI::encode(params['q'].encode('GBK'))
+    url = "http://s.readnovel.com/web/search.php?searchconditions=0&keywords=#{query}&button=%CB%D1+%CB%F7"
+    docuri = URI.parse(url)
+    doc = nil
+    Net::HTTP.start docuri.host, docuri.port do |http|
+      res = http.get(docuri.path)
+      doc = Nokogiri::HTML(res.body)
+    end
+    doc.css("#sortListPic").child
   end
 
   def show
@@ -7,4 +16,5 @@ class ResultsController < ApplicationController
 
   def update
   end
+
 end
